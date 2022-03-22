4c5d271c003611c837ef3f86e9b0c3f40ba3e34b
dyaapriLita <101492097+dyaapriLita@users.noreply.github.com>
2022-03-22 11:19:22 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+import Alert from './components/Alert';
+import Media from './components/Media';
+// import element Alert yang telah dibuat
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      type: "success",
+      header: "React JS itu",
+      content: "seru dan menyenangkan"
+    }
+  }
+   // fungsi untuk mengubah tipe
+   changeTypeAlert = (event) => {
+    this.setState({ type: event.target.value });
+  }
+  // fungsi untuk mengubah header
+  changeHeaderAlert = (event) => {
+    this.setState({ header: event.target.value });
+  }
+  // fungsi untuk mengubah content
+  changeContentAlert = (event) => {
+    this.setState({ content: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App container col-sm-6">
+        <Alert type={this.state.type} header={this.state.header}>
+          {this.state.content}
+        </Alert>
+        <hr />
+        <h4>Alert Control</h4>
+        <b className="text-left">Tipe Alert</b>
+        <select className="form-control" name="type" value={this.state.type}
+          onChange={this.changeTypeAlert}>
+          <option value="success">Success</option>
+          <option value="warning">Warning</option>
+          <option value="danger">Danger</option>
+          <option value="info">Info</option>
+        </select>
+        <b className="text-left">Header Alert</b>
+        <input
+          type="text" name="header" className="form-control" value={this.state.header}
+          onChange={this.changeHeaderAlert} />
+        <b className="text-left">Content Alert</b>
+        <input
+          type="text" name="content" className="form-control" value={this.state.content}
+          onChange={this.changeContentAlert} />
+        <Alert type="danger" header="Fatal Error">
+          Ini adalah alert dengan tipe danger
+        </Alert>
+        <Alert type="success" header="Berhasil">
+          Ini adalah alert dengan tipe success
+        </Alert>
+        <Alert type="warning" header="Warning">
+          Ini adalah alert dengan tipe warning
+        </Alert>
+        <Media image="lita1.jpg" title="Doy Terkejut">
+          melihat salak sekebon
+        </Media>
+        <Media image="lita2.jpg" title="Doy pawang hujan">
+          diberi gaji salak sekebon
+        </Media>
+      </div>
+    );
+  }
+}
+
+export default App;
